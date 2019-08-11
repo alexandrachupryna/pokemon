97df6b456028fb81c0461ed06065317616ef6f90
alexandrachupryna <alexandrachuprina93@gmail.com>
2019-08-11 20:56:42 +0300

    Updates

---
+++ precache-manifest.de1ab50d26f96e8d632ded1d50819008.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "32e722ad99e01964e17fb684aa5e98ab",
+    "url": "/pokemon/index.html"
+  },
+  {
+    "revision": "48df5dbc653238bd9556",
+    "url": "/pokemon/static/css/main.519e9c4a.chunk.css"
+  },
+  {
+    "revision": "c55688aa01a3f75fc0d2",
+    "url": "/pokemon/static/js/2.334e6877.chunk.js"
+  },
+  {
+    "revision": "48df5dbc653238bd9556",
+    "url": "/pokemon/static/js/main.47b7ce66.chunk.js"
+  },
+  {
+    "revision": "d510b31a3114b62b4c50",
+    "url": "/pokemon/static/js/runtime~main.04b524e9.js"
+  }
+]);
